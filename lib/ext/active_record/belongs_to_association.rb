02c5292ee9756297687f5e09e9093ca80e4dcd65
Pat Maddox <pat.maddox@gmail.com>
2008-12-10 16:29:39 -0800

    handle save, save!, update_attribute, and update_attributes for belongs_to

--- lib/ext/active_record/belongs_to_association.rb
+++ lib/ext/active_record/belongs_to_association.rb
@@ -1,11 +1,30 @@
 ActiveRecord::Associations::BelongsToAssociation.class_eval do
-  def update_attribute(attribute, new_value)
-    old_id = proxy_target.id
-    result = proxy_target.update_attribute attribute, new_value
-    new_id = proxy_target.id
-    if new_id != old_id
-      proxy_owner.update_attribute @reflection.primary_key_name, new_id
+  def self.delegate_to_proxy_with_value_object_checking(*method_names)
+    method_names.each do |method_name|
+      arg_names = []
+      arity = ActiveRecord::Base.instance_method(method_name).arity
+      if arity == -1
+        arg_names << "*args"
+      else
+        arity.times {|i| arg_names << "arg#{i}" }
+      end
+      arg_names << "&block"
+      args = arg_names.join ', '
+      
+      definition = <<-METHOD_DEF
+        def #{method_name}(#{args})
+          old_id = proxy_target.id
+          result = proxy_target.send(:#{method_name}, #{args})
+          new_id = proxy_target.id
+          if new_id != old_id
+            proxy_owner.update_attribute @reflection.primary_key_name, new_id
+          end
+          result
+        end
+      METHOD_DEF
+      class_eval definition
     end
-    result
   end
+
+  delegate_to_proxy_with_value_object_checking :save, :save!, :update_attribute, :update_attributes
 end
--- spec/acts_as_value_object_spec.rb
+++ spec/acts_as_value_object_spec.rb
@@ -41,11 +41,38 @@ describe "acts_as_value_object" do
       acts_as_value_object
     end
 
+    before(:each) do
+      @email = Email.create! :address => "joe@example.com", :system_id => 123
+      @order = Order.create! :email => @email
+    end
+
+    it "should update the reference when changed with save" do
+      @order.email.address = "newaddress@example.com"
+      @order.email.save
+      @order.reload.email.address.should == "newaddress@example.com"
+    end    
+
+    it "should update the reference when changed with save" do
+      @order.email.address = "newaddress@example.com"
+      @order.email.save!
+      @order.reload.email.address.should == "newaddress@example.com"
+    end    
+
     it "should update the reference when changed with update_attribute" do
-      email = Email.create! :address => "joe@example.com", :system_id => 123
-      order = Order.create! :email => email
-      order.email.update_attribute :address, "newaddress@example.com"
-      order.reload.email.address.should == "newaddress@example.com"
+      @order.email.update_attribute :address, "newaddress@example.com"
+      @order.reload.email.address.should == "newaddress@example.com"
+    end
+
+    it "should update the reference when changed with update_attributes" do
+      @order.email.update_attributes :address => "newaddress@example.com"
+      @order.reload.email.address.should == "newaddress@example.com"
+    end
+
+    [:save, :save!, :update_attribute, :update_attributes].each do |method_name|
+      it "should not screw with the arity of #{method_name}" do
+        ActiveRecord::Associations::BelongsToAssociation.instance_method(method_name).arity.should ==
+          ActiveRecord::Base.instance_method(method_name).arity
+      end
     end
   end
 end
